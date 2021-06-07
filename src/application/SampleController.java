9e2061c1dc91638e854a420cc0dd4d034ab51788
Sumit Chutani <sumitengineer23@gmail.com>
2021-06-07 20:05:23 +0530

    Added the font styling options for the text like bold,italic etc.

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -6,10 +6,14 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.Optional;
 
 import colordialog.ColorDialogController;
+import feedbackform.FeedbackController;
+import findAndReplaceDialog.FindAndReplaceController;
 import fontdialog.FontDialogController;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -20,234 +24,352 @@ import javafx.scene.control.Dialog;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import searchHandle.SearchController;
 import sizedialog.SizeDialogController;
 
 public class SampleController {
-	
+
 	@FXML
 	private BorderPane mainPane;
-	
+
 	@FXML
 	private TextArea mainTextArea;
-	
+
 	private Stage currentStage;
-	
+
 	private File currentFile;
-	
-	private String currentFont;
-	
-	private static final FileChooser fileChooser=new FileChooser();
-	
+
+	private String currentFont = "Times New Roman", currentColor = "black";
+	int currentSize = 16;
+
+	private static final FileChooser fileChooser = new FileChooser();
+
 	static {
-		fileChooser.setInitialDirectory(
-				new File(System.getProperty("user.home")));
-		
+		fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
 		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text", "*.txt"),
-				new FileChooser.ExtensionFilter("Java", "*.java"),
-				new FileChooser.ExtensionFilter("C", "*.c"),
-				new FileChooser.ExtensionFilter("C++", "*.cpp"),
-				new FileChooser.ExtensionFilter("All Files","*.*"));
-		
+				new FileChooser.ExtensionFilter("Java", "*.java"), new FileChooser.ExtensionFilter("C", "*.c"),
+				new FileChooser.ExtensionFilter("C++", "*.cpp"), new FileChooser.ExtensionFilter("All Files", "*.*"));
+
 	}
-	
+
 	public void initialize() {
 		mainTextArea.setStyle("-fx-text-fill:black;");
-		mainTextArea.setFont(Font.font("Times New Roman",16));
+		mainTextArea.setFont(Font.font("Times New Roman", 16));
 	}
-	
+
 	@FXML
 	public void createSpace() {
 		mainTextArea.clear();
-		currentFile=null;
-		
-		if(currentFile==null) {
-			currentStage=(Stage)mainPane.getScene().getWindow();
+		currentFile = null;
+
+		if (currentFile == null) {
+			currentStage = (Stage) mainPane.getScene().getWindow();
 		}
 		currentStage.setTitle("Notepad");
 	}
-	
+
 	@FXML
 	public void showOpenDialog() {
 		fileChooser.setTitle("Notepad");
-		File selectedFile=fileChooser.showOpenDialog(mainPane.getScene().getWindow());
-		currentFile=selectedFile;
-		if(currentFile!=null) {
+		File selectedFile = fileChooser.showOpenDialog(mainPane.getScene().getWindow());
+		currentFile = selectedFile;
+		if (currentFile != null) {
 			readFile(selectedFile);
 		}
-		
-		if(currentStage==null) {
-			currentStage=(Stage)mainPane.getScene().getWindow();
+
+		if (currentStage == null) {
+			currentStage = (Stage) mainPane.getScene().getWindow();
 		}
-		
-		if(selectedFile!=null) {
+
+		if (selectedFile != null) {
 			currentStage.setTitle(selectedFile.getName());
 		}
 	}
-	
+
 	@FXML
 	public void saveFile() {
-		if(currentFile!=null) {
-			String location=currentFile.getAbsolutePath();
+		if (currentFile != null) {
+			String location = currentFile.getAbsolutePath();
 			writeInFile(location);
-		}
-		else {
-			Alert alert=new Alert(Alert.AlertType.ERROR);
+		} else {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
 			alert.initOwner(mainPane.getScene().getWindow());
 			alert.setTitle("Error!");
 			alert.setHeaderText("No file selected!");
 			alert.setContentText("Please choose Save option");
 			alert.showAndWait();
-			
+
 		}
 	}
-	
+
 	@FXML
 	public void showSaveAsDialog() {
 		fileChooser.setTitle("Save as");
-		File savedFile=fileChooser.showSaveDialog(mainPane.getScene().getWindow());
-		currentFile=savedFile;
+		File savedFile = fileChooser.showSaveDialog(mainPane.getScene().getWindow());
+		currentFile = savedFile;
 		writeInFile(savedFile.getAbsolutePath());
 		currentStage.setTitle(savedFile.getName());
 	}
-	
+
 	@FXML
 	public void handleExit() {
 		Platform.exit();
 	}
-	
+
 	@FXML
-	public void handleWordWrap() {
-		if(mainTextArea.isWrapText()) {
-			mainTextArea.setWrapText(false);
+	public void handleSearch() {
+		Dialog<ButtonType> dialog = new Dialog<ButtonType>();
+		dialog.initOwner(mainPane.getScene().getWindow());
+		dialog.setTitle("Search Box");
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		fxmlLoader.setLocation(getClass().getResource("/searchHandle/SearchDialog.fxml"));
+		try {
+			dialog.getDialogPane().setContent(fxmlLoader.load());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		SearchController controller = new SearchController();
+
+		dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		Optional<ButtonType> result = dialog.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			String url = controller.getUrl();
+			Desktop desktop = Desktop.getDesktop();
+			try {
+				desktop.browse(new URL(url).toURI());
+				Alert alert = new Alert(Alert.AlertType.INFORMATION);
+				alert.initOwner(mainPane.getScene().getWindow());
+				alert.setTitle("Redirecting to the website...");
+				alert.setHeaderText("Loading ⌛⌛⌛");
+				alert.showAndWait();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@FXML
+	public void handleFindAndReplace() {
+		Dialog<ButtonType> dialog = new Dialog<ButtonType>();
+		dialog.initOwner(mainPane.getScene().getWindow());
+		dialog.setTitle("Find & Replace");
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		fxmlLoader.setLocation(getClass().getResource("/findAndReplaceDialog/FindAndReplaceDialog.fxml"));
+		try {
+			dialog.getDialogPane().setContent(fxmlLoader.load());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		FindAndReplaceController controller = new FindAndReplaceController();
+
+		dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		Optional<ButtonType> result = dialog.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+
+		}
+	}
+	
+	public int carryOutSearch(String text) {
+		if(text.trim().length()==0) {
+			return -1;
 		}
 		else {
+			String mainText=mainTextArea.getText();
+			System.out.println(mainText);
+//			int index=mainText.indexOf(text);
+			return 0;
+		}
+	}
+	
+	public boolean carryOutReplace(String text,String altText) {
+		if(text.trim().length()==0||altText.trim().length()==0) {
+			return false;
+		}
+		mainTextArea.getText().replaceFirst(text, altText);
+		return true;
+	}
+
+	@FXML
+	public void handleWordWrap() {
+		if (mainTextArea.isWrapText()) {
+			mainTextArea.setWrapText(false);
+		} else {
 			mainTextArea.setWrapText(true);
 		}
 	}
-	
+
 	@FXML
 	public void showFontDialog() {
-		
-		Dialog<ButtonType> dialog=new Dialog<ButtonType>();
+
+		Dialog<ButtonType> dialog = new Dialog<ButtonType>();
 		dialog.initOwner(mainPane.getScene().getWindow());
 		dialog.setTitle("Change Font");
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("/fontdialog/fontDialog.fxml"));
-        try {
-            dialog.getDialogPane().setContent(fxmlLoader.load());
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-        FontDialogController controller = new FontDialogController();
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		fxmlLoader.setLocation(getClass().getResource("/fontdialog/fontDialog.fxml"));
+		try {
+			dialog.getDialogPane().setContent(fxmlLoader.load());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		FontDialogController controller = new FontDialogController();
 
-        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
-        Optional<ButtonType> result = dialog.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK) {
-            currentFont = controller.getFont();
-            Font font = Font.font(currentFont, 16);
-            mainTextArea.setFont(font);
-        }
+		dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		Optional<ButtonType> result = dialog.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			currentFont = controller.getFont();
+			Font font = Font.font(currentFont, 16);
+			mainTextArea.setFont(font);
+		}
 	}
-	
+
 	@FXML
 	public void showColorDialog() {
-		
-		Dialog<ButtonType> dialog=new Dialog<ButtonType>();
+
+		Dialog<ButtonType> dialog = new Dialog<ButtonType>();
 		dialog.initOwner(mainPane.getScene().getWindow());
 		dialog.setTitle("Change Color");
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("/colordialog/ColorDialog.fxml"));
-        try {
-            dialog.getDialogPane().setContent(fxmlLoader.load());
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-        ColorDialogController controller = new ColorDialogController();
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		fxmlLoader.setLocation(getClass().getResource("/colordialog/ColorDialog.fxml"));
+		try {
+			dialog.getDialogPane().setContent(fxmlLoader.load());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		ColorDialogController controller = new ColorDialogController();
 
-        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
-        Optional<ButtonType> result = dialog.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK) {
-        	String currentColor=controller.getColor();
-        	mainTextArea.setStyle("-fx-text-fill: #"+currentColor.substring(2,8)+";");
-        }
+		dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		Optional<ButtonType> result = dialog.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			currentColor = controller.getColor();
+			mainTextArea.setStyle("-fx-text-fill: #" + currentColor.substring(2, 8) + ";");
+		}
 	}
-	
+
 	@FXML
 	public void showSizeDialog() {
-		
-		Dialog<ButtonType> dialog=new Dialog<ButtonType>();
+
+		Dialog<ButtonType> dialog = new Dialog<ButtonType>();
 		dialog.initOwner(mainPane.getScene().getWindow());
 		dialog.setTitle("Change size");
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("/sizedialog/SizeDialog.fxml"));
-        try {
-            dialog.getDialogPane().setContent(fxmlLoader.load());
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-        SizeDialogController controller = new SizeDialogController();
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		fxmlLoader.setLocation(getClass().getResource("/sizedialog/SizeDialog.fxml"));
+		try {
+			dialog.getDialogPane().setContent(fxmlLoader.load());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		SizeDialogController controller = new SizeDialogController();
 
-        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
-        Optional<ButtonType> result = dialog.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK) {
-        	int currentSize=controller.getSize();
-        	Font font=new Font(currentFont,currentSize);
-        	mainTextArea.setFont(font);
-        }
+		dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		Optional<ButtonType> result = dialog.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			currentSize = controller.getSize();
+			Font font = new Font(currentFont, currentSize);
+			mainTextArea.setFont(font);
+		}
 	}
-	
+
+	@FXML
+	public void handleRegularFont() {
+		mainTextArea.setFont(Font.font(currentFont, FontWeight.NORMAL, currentSize));
+	}
+
+	@FXML
+	public void handleBoldFont() {
+		mainTextArea.setFont(Font.font(currentFont, FontWeight.BOLD, currentSize));
+	}
+
+	@FXML
+	public void handleItalicFont() {
+		mainTextArea.setFont(Font.font(currentFont, FontPosture.ITALIC, currentSize));
+	}
+
 	@FXML
 	public void handleAbout() {
-		Desktop desktop=Desktop.getDesktop();
+		Desktop desktop = Desktop.getDesktop();
 		try {
 			desktop.browse(new URL("https://github.com/sumit-codebrewer").toURI());
-			Alert alert=new Alert(Alert.AlertType.INFORMATION);
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
 			alert.initOwner(mainPane.getScene().getWindow());
-			//Stage stage=(Stage)alert.getDialogPane().getScene().getWindow();
+			// Stage stage=(Stage)alert.getDialogPane().getScene().getWindow();
 			alert.setTitle("About Creator");
 			alert.setHeaderText("Notepad is developed by:");
 			alert.setContentText("Sumit-Codebrewer");
 			alert.showAndWait();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	@FXML
+	public void handleFeedback() throws IOException {
+		Dialog<ButtonType> dialog = new Dialog<ButtonType>();
+		dialog.initOwner(mainPane.getScene().getWindow());
+		dialog.setTitle("Feedback Form");
+		dialog.setHeaderText("Give your valuable feedback here");
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		fxmlLoader.setLocation(getClass().getResource("/feedbackform/FeedbackDialog.fxml"));
+		try {
+			dialog.getDialogPane().setContent(fxmlLoader.load());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		FeedbackController controller = new FeedbackController();
+
+		dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		Optional<ButtonType> result = dialog.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			String name = controller.getName();
+			String review = controller.getReview();
+			String suggestions = controller.getSuggestions();
+			try (FileWriter f = new FileWriter("feedbacks.txt", true);
+					BufferedWriter b = new BufferedWriter(f);
+					PrintWriter p = new PrintWriter(b);) {
+				p.print(String.format("Name:%s\tReview:%s\t Suggestions:%s\n",name,review,suggestions));
+				p.println();
+			} catch (IOException i) {
+				i.printStackTrace();
+			}
+
+		}
+	}
+
 	public void readFile(File selectedFile) {
-		new Thread(()->{
-			StringBuilder sb=new StringBuilder();
-			try(BufferedReader br=new BufferedReader(new FileReader(selectedFile))){
+		new Thread(() -> {
+			StringBuilder sb = new StringBuilder();
+			try (BufferedReader br = new BufferedReader(new FileReader(selectedFile))) {
 				String s;
-				while((s=br.readLine())!=null) {
+				while ((s = br.readLine()) != null) {
 					sb.append(s).append("\n");
 				}
 				mainTextArea.setText(sb.toString());
-			}
-			catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
 		}).start();
-		
-		
+
 	}
-	
-	
+
 	public void writeInFile(String location) {
-		new Thread(()->{
-			try(BufferedWriter bw=new BufferedWriter(new FileWriter(new File(location)))){
-				String text=mainTextArea.getText();
+		new Thread(() -> {
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(location)))) {
+				String text = mainTextArea.getText();
 				bw.write(text);
-			}
-			catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
 		}).start();
-		
+
 	}
 }
