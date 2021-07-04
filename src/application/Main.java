4d9a75af0a57f9be66ee23b1898c220fd10b506d
Sumit Chutani <71552669+sumit-codebrewer@users.noreply.github.com>
2021-07-04 12:42:29 +0530

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
